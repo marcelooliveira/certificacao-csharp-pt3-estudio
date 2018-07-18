56d4e8105330f95cf4fda9ee05056cca099900de
marcelooliveira <mclricardo@gmail.com>
2018-07-18 14:59:12 -0300

    965-CertificacaoCSharpParte3-video1.2-Encapsulamento

--- certificacao-csharp-pt3/certificacao-csharp-pt3/Program.cs
+++ certificacao-csharp-pt3/certificacao-csharp-pt3/Program.cs
@@ -10,6 +10,37 @@ namespace Topico1
     {
         static void Main(string[] args)
         {
+            Funcionario funcionario = new Funcionario();
+            //funcionario.salario = 1000;
+            //Console.WriteLine(funcionario.salario);
+
+            //funcionario.salario = -1200;
+            //Console.WriteLine(funcionario.salario);
+
+            //funcionario.Salario = -1200;
+            funcionario.Salario = 1200;
+            Console.WriteLine(funcionario.Salario);
+        }
+    }
+
+    class Funcionario
+    {
+        decimal salario;
+
+        public decimal Salario //encapsulamento do campo salario
+        {
+            get
+            {
+                return salario;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("salário não pode ser negativo");
+                }
+                salario = value;
+            }
         }
     }
 }
