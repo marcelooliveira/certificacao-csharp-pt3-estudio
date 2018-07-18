e07925d14da03d11b95844ae42d9b921b535293f
marcelooliveira <mclricardo@gmail.com>
2018-07-18 17:51:33 -0300

    .

---
+++ certificacao-csharp-pt3/Topico2.Projetar Interface/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topico2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Televisao televisao = new Televisao();
+            Abajur abajur = new Abajur();
+            Lanterna lanterna = new Lanterna();
+            Radio radio = new Radio();
+        }
+    }
+
+    class Televisao
+    {
+        public event EventHandler Ligou;
+        public event EventHandler Desligou;
+
+        public double FrequenciaDoCanal { get; set; }
+
+        public void Desligar()
+        {
+        }
+
+        public void Ligar()
+        {
+            if (Ligou != null)
+            {
+                Ligou(this, new EventArgs());
+            }
+        }
+    }
+
+    class Abajur
+    {
+        public int PotenciaDaLampada { get; set; }
+
+        public event EventHandler Ligou;
+        public event EventHandler Desligou;
+
+        public void Desligar()
+        {
+        }
+
+        public void Ligar()
+        {
+        }
+    }
+
+    class Lanterna
+    {
+        public double PotenciaDaLampada { get; set; }
+
+        public event EventHandler Ligou;
+        public event EventHandler Desligou;
+
+        public void Desligar()
+        {
+        }
+
+        public void Ligar()
+        {
+        }
+    }
+
+    class Radio
+    {
+        public event EventHandler Ligou;
+        public event EventHandler Desligou;
+
+        public double FrequenciaDaEstacao { get; set; }
+
+        public void Desligar()
+        {
+        }
+
+        public void Ligar()
+        {
+        }
+    }
+
+}
