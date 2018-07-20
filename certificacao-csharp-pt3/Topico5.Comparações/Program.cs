a4d4d5a86391e6eba2ffac004a6bfb5b7c7aa02e
marcelo oliveira <mclricardo@gmail.com>
2018-07-20 15:57:57 -0300

    965-CertificacaoCSharpParte3-video5.1-Equals.trec

--- certificacao-csharp-pt3/Topico5.Comparações/Program.cs
+++ certificacao-csharp-pt3/Topico5.Comparações/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Topico5.Comparações
 {
@@ -23,6 +24,9 @@ namespace Topico5.Comparações
                 Nome = "josé da silva",
                 DataNascimento = new DateTime(1990, 1, 1)
             };
+
+            Console.WriteLine(aluno1.Equals(aluno2));
+            Console.WriteLine(aluno1.Equals(aluno3));
         }
     }
 
@@ -34,6 +38,25 @@ namespace Topico5.Comparações
         public override string ToString()
         {
             return $"Nome: {Nome}, Data nascimento: {DataNascimento:dd/MM/yyyy}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Aluno outro = obj as Aluno;
+            if (outro == null)
+            {
+                return false;
+            }
+            return this.Nome.Equals(outro.Nome, StringComparison.CurrentCultureIgnoreCase)
+                && this.DataNascimento.Equals(outro.DataNascimento);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1523756618;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + DataNascimento.GetHashCode();
+            return hashCode;
         }
     }
 }
