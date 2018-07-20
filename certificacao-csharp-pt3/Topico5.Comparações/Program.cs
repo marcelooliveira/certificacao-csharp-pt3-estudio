4a70d74c6c5819faf283146588a110bb053ee05d
marcelo oliveira <mclricardo@gmail.com>
2018-07-20 17:45:00 -0300

    965-CertificacaoCSharpParte3-video5.2-IComparable.trec

--- certificacao-csharp-pt3/Topico5.Comparações/Program.cs
+++ certificacao-csharp-pt3/Topico5.Comparações/Program.cs
@@ -27,10 +27,38 @@ namespace Topico5.Comparações
 
             Console.WriteLine(aluno1.Equals(aluno2));
             Console.WriteLine(aluno1.Equals(aluno3));
+
+            Aluno aluno4 = new Aluno
+            {
+                Nome = "ANDRÉ DOS SANTOS",
+                DataNascimento = new DateTime(1970, 1, 1)
+            };
+
+            Aluno aluno5 = new Aluno
+            {
+                Nome = "Ana de Souza",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
+
+            List<Aluno> alunos = new List<Aluno>
+            {
+                aluno1,
+                aluno2,
+                aluno3,
+                aluno4,
+                aluno5
+            };
+
+            alunos.Sort();
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
         }
     }
 
-    class Aluno
+    class Aluno : IComparable
     {
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
@@ -57,6 +85,31 @@ namespace Topico5.Comparações
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
             hashCode = hashCode * -1521134295 + DataNascimento.GetHashCode();
             return hashCode;
+        }
+
+        public int CompareTo(object obj)
+        {
+            //retorna 0 => objetos são iguais
+            //retornar > 0 => objeto atual vem depois
+            //retornar < 0 => objeto atual vem antes
+
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            Aluno outro = obj as Aluno;
+            if (outro == null)
+            {
+                throw new ArgumentException("Objeto não é um Aluno");
+            }
+
+            int resultado = this.DataNascimento.CompareTo(outro.DataNascimento);
+            if (resultado == 0)
+            {
+                resultado = this.Nome.CompareTo(outro.Nome);
+            }
+            return resultado;
         }
     }
 }
